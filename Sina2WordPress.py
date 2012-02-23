6187a9857e5aaf377cdb91d30458e4753ab9db61
huxuan <i@huxuan.org>
2012-02-23 18:26:14 +0800

    Reconstruction of Sina2WordPress:
    1) support Both CLI and GUI by Sina2WordPress.py
    2) reconstruct old main.py to importable Sina2WordPressCore.py
    3) various bugs fixed
    4) (TBD) Progress Report both for GUI and CLI
    5) (TBD) Code Clean and fullfill comments

---
+++ Sina2WordPress.py
@@ -0,0 +1,216 @@
+#!/usr/bin/python
+#encoding: utf8
+# +-----------------------------------------------------------------------------
+# | File: Sina2WordPressCLI.py
+# | Author: huxuan
+# | E-mail: i(at)huxuan.org
+# | Created: 2012-02-23
+# | Last modified: 2012-02-23
+# | Description:
+# |     Sina2WordPress with Command Line Interface
+# |
+# | Copyrgiht (c) 2012 by huxuan. All rights reserved.
+# | License GPLv3
+# +-----------------------------------------------------------------------------
+
+import sys
+import time
+
+import wx
+
+import Sina2WordPressCore
+
+SLEEP_TIME = 2
+
+class Sina2WordPress():
+    """Summary of Sina2WordPress
+
+    Attributes:
+        args: Description of args
+    """
+    def __init__(self, sina_url, wordpress_admin, wordpress_url):
+        """Init Sina2WordPress"""
+        self.output_file = wordpress_admin + '.xml'
+        f = file(self.output_file, 'w')
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n\n<rss version="2.0"\n\txmlns:excerpt="http://wordpress.org/export/1.1/excerpt/"\n\txmlns:content="http://purl.org/rss/1.0/modules/content/"\n\txmlns:wfw="http://wellformedweb.org/CommentAPI/"\n\txmlns:dc="http://purl.org/dc/elements/1.1/"\n\txmlns:wp="http://wordpress.org/export/1.1/"\n>\n\n<channel>\n<wp:wxr_version>1.1</wp:wxr_version>\n')
+        f.close()
+
+        print sina_url
+        content_url, sina_admin = Sina2WordPressCore.indexAnalyze(sina_url)
+        
+        posts_urls = []
+        while True:
+            print content_url
+            time.sleep(SLEEP_TIME / 2)
+
+            posts_page_urls, content_url = Sina2WordPressCore.contentAnalyze(content_url) 
+            posts_urls.extend(posts_page_urls)
+            if not content_url:
+                break
+        posts_urls.reverse()
+
+        comment_id = 0
+        for post_url in posts_urls[4:]:
+            time.sleep(SLEEP_TIME)
+            #Todo(huxuan): Show post Progress here
+            print '(%d/%d)' % (posts_urls.index(post_url) + 1, len(posts_urls)) ,post_url
+            post_text, comment_status = Sina2WordPressCore.postAnalyze(post_url, wordpress_admin)
+            text = [post_text]
+
+            if comment_status == 'open': 
+                key = post_url.replace(r'http://blog.sina.com.cn/s/blog_','').replace(r'.html','')
+                num = 0
+
+                while True:
+                    time.sleep(SLEEP_TIME)
+
+                    num += 1
+                    comment_url = r'http://blog.sina.com.cn/s/comment_%s_%d.html' %(key, num)
+                    print comment_url
+
+                    comment_text, comment_id = Sina2WordPressCore.commentAnalyze(
+                            comment_url, comment_id, sina_admin, wordpress_admin, wordpress_url)
+
+                    if comment_text:
+                        text.append(comment_text)
+                    else:
+                        break
+
+            text.append('\n</item>')
+            self.output(''.join(text))
+
+        self.output('\n</channel>\n</rss>')
+
+        print 'Succeed !'
+    
+    def output(self, text):
+        """docstring for print2file"""
+        f = file(self.output_file, 'a')
+        f.write(text)
+        f.close()
+
+class Sina2WordPressInterface(object):
+    """Graphic User Interface for Sina2WordPress
+    """
+    def __init__(self, *args):
+        """Init Sina2WordPressGUI"""
+        pass
+
+    def ShowProgress(self, *args, **kwargs):
+        """docstring for ShowProgress"""
+        pass
+
+    def ShowError(self, msg):
+        """Summary of ShowError
+        """
+        pass
+
+class Sina2WordPressCLI(Sina2WordPressInterface):
+    """Graphic User Interface for Sina2WordPress
+    """
+    def __init__(self, *args):
+        """Init Sina2WordPressGUI"""
+        Sina2WordPress(*args)
+
+    def ShowProgress(self, *args, **kwargs):
+        """docstring for ShowProgress"""
+        pass
+
+    def ShowError(self, msg):
+        """Summary of ShowError
+        """
+        pass
+
+class Sina2WordPressGUI(Sina2WordPressInterface):
+    """Graphic User Interface for Sina2WordPress
+    """
+    def __init__(self, *args):
+        """Init Sina2WordPressGUI"""
+        self.app = wx.App()
+        self.window = Sina2WordPressWindow(None, title='Sina2WordPress')
+        self.app.MainLoop()
+
+    def ShowProgress(self, *args, **kwargs):
+        """docstring for ShowProgress"""
+        pass
+
+    def ShowError(self, msg):
+        """Summary of ShowError
+        """
+        pass
+
+class Sina2WordPressWindow(wx.Frame):
+    """Window of Sina2WordPressGUI
+    """
+    def __init__(self, *args, **kwargs):
+        """Init Sina2WordPressWindow"""
+        super(Sina2WordPressWindow, self).__init__(*args, **kwargs)
+
+        mainSizer = wx.FlexGridSizer(rows=4, cols=2)
+
+        self.sina_url_label = wx.StaticText(self, label='sina_url (e.g. http://blog.sina.com.cn/twoclod)')
+        self.sina_url_value = wx.TextCtrl(self)
+
+        self.wordpress_admin_label = wx.StaticText(self, label='wordpress_admin: (e.g. admin)')
+        self.wordpress_admin_value = wx.TextCtrl(self)
+
+        self.wordpress_url_label = wx.StaticText(self, label='wordpress_url: (e.g. http://huxuan.org/)')
+        self.wordpress_url_value = wx.TextCtrl(self)
+
+        self.help_button = wx.Button(self, label='帮助(help)')
+        self.run_button = wx.Button(self, label="运行(run)")
+
+        mainSizer.Add(self.sina_url_label)
+        mainSizer.Add(self.sina_url_value)
+
+        mainSizer.Add(self.wordpress_admin_label)
+        mainSizer.Add(self.wordpress_admin_value)
+
+        mainSizer.Add(self.wordpress_url_label)
+        mainSizer.Add(self.wordpress_url_value)
+
+        mainSizer.Add(self.help_button)
+        mainSizer.Add(self.run_button)
+
+        self.Bind(wx.EVT_BUTTON, self.ShowHelp, self.help_button)
+        self.Bind(wx.EVT_BUTTON, self.CallCore, self.run_button)
+
+        self.SetSizerAndFit(mainSizer)
+        self.Show(True)
+
+    def CallCore(self, event):
+        """Call the core convert process
+        """
+        args = [self.sina_url_value.GetValue(),
+                self.wordpress_admin_value.GetValue(),
+                self.wordpress_url_value.GetValue(), ]
+        Sina2WordPress(*args)
+        self.Close(True)
+
+    def ShowHelp(self, event):
+        """Show help info dialog
+        """
+        content = 'http://github.com/huxuan/sina2wordpress'
+        helpDialog = Sina2WordPressMessageDialog(self, content, 'Help', wx.OK)
+
+class Sina2WordPressMessageDialog(wx.MessageDialog):
+    """MessageDialog of Sinaw2WordPress
+    """
+    def __init__(self, *args, **kwargs):
+        """Init Sina2WordPressMessageDialog"""
+        super(Sina2WordPressMessageDialog, self).__init__(*args, **kwargs)
+        self.ShowModal()
+        self.Destroy()
+
+def main():
+    """Summary of main
+    """
+    if len(sys.argv) == 4:
+        Sina2WordPressCLI(*sys.argv[1:4])
+    elif len(sys.argv) == 1:
+        Sina2WordPressGUI()
+    else:
+        print '[Error] The number of parameters is invalid!'
+
+if __name__ == '__main__':
+    main()
---
+++ Sina2WordPressCore.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+#encoding: utf8
+# +-----------------------------------------------------------------------------
+# | File: Sina2WordPress.py
+# | Author: huxuan
+# | E-mail: i(at)huxuan.org
+# | Created: 2011-02-21
+# | Last modified: 2012-02-23
+# | Description:
+# |     Core Process of Sina2WordPress
+# |     Analyze Sina Blog and convert it to WXR for WordPress
+# |
+# | Copyrgiht (c) 2012 by huxuan. All rights reserved.
+# | License GPLv3
+# +-----------------------------------------------------------------------------
+
+import re
+import json
+import urllib
+import urllib2
+import cookielib
+
+headers = {'Referer':'http://blog.sina.com.cn/', 'User-Agent':'Opera/9.60',}
+
+sina_admin_pattern = re.compile(r'<title>(.*?)_新浪博客</title>')
+contents_pattern = re.compile(r'<a +href="(.*?)">博文目录</a>')
+post_pattern=re.compile(r'<a +title.*?href="(.*?)">.*?</a>')
+nextpage_pattern=re.compile(r'<li class="SG_pgnext"><a href="(.*?)".*</li>')
+post_title_pattern=re.compile(r'<h2.*?titName SG_txta">(.*?)</h2>')
+post_time_pattern=re.compile(r'<span class="time SG_txtc">[(](.*?)[)]</span>')
+post_content_pattern=re.compile(r'<!-- 正文开始 -->(.*?)(<INS>|<!-- 正文结束 -->)', re.S)
+content_replace_pattern=re.compile(r'<wbr>|<span class=\'MASSf21674ffeef7\'>.*?</span>|<p STYLE="TexT-inDenT: 2em">&nbsp;<wbr></P>|<div>&nbsp;<wbr></DIV>| STYLE="TexT-inDenT: 2em"|<div id="sina_keyword_ad_area2" class="articalContent  ">', re.S)
+content_space_replace_pattern=re.compile(r'&nbsp;')
+post_category_pattern=re.compile(r'<span class="SG_txtb">分类：</span>.*?<a.*?>(.*?)</a>', re.S)
+post_tags_pattern=re.compile(r'\$tag=\'(.*?)\'')
+comment_author_pattern=re.compile(r'<span class="SG_revert_Tit".*?>(.*?)</span>')
+comment_url_pattern=re.compile(r'<a href="(.*?)" target="_blank">(.*?)</a>')
+comment_time_pattern=re.compile(r'<em class="SG_txtc">(.*?)</em>')
+comment_content_pattern=re.compile(r'(?:<div class="SG_revert_Inner SG_txtb".*?>|<p class="myReInfo wordwrap">)(.*?)</div>', re.S)
+
+def commentAnalyze(url, comment_id, sina_admin, wordpress_admin,wordpress_url):
+    """Analyze the comment page including decode via json.loads() and regular expression"""
+
+    #headers['Referer'] = url
+    request = urllib2.Request(url, None, headers)
+    page=urllib2.urlopen(request).read().replace('data:','\"data\":',1)
+
+    if 'noCommdate' in page:
+        return None, comment_id
+    
+    data=json.loads(page)['data']
+    author=comment_author_pattern.findall(data)
+    comment_time=comment_time_pattern.findall(data)
+    content=comment_content_pattern.findall(data)
+    text = []
+    for i in range(len(author)):
+        parent = 0
+        url = ''
+        if author[i] == u'博主回复：' or sina_admin in author[i]:
+            parent = comment_id
+            author[i] = wordpress_admin
+            url = wordpress_url
+        else:
+            result=comment_url_pattern.search(author[i])
+            if result:
+                url = result.group(1)
+                author[i] = result.group(2)
+        comment_id += 1
+        content[i] = content_replace_pattern.sub('', content[i])
+        content[i] = content_space_replace_pattern.sub(' ', content[i])
+        text.append('\n\t<wp:comment>\n\t\t<wp:comment_id>'+str(comment_id)+'</wp:comment_id>\n\t\t<wp:comment_author><![CDATA['+author[i].encode('utf8')+']]></wp:comment_author>\n\t\t<wp:comment_author_url>'+url.encode('utf8')+'</wp:comment_author_url>\n\t\t<wp:comment_date>'+comment_time[i].encode('utf8')+'</wp:comment_date>\n\t\t<wp:comment_content><![CDATA['+content[i].encode('utf8')+']]></wp:comment_content>\n\t\t<wp:comment_approved>1</wp:comment_approved>\n\t\t<wp:comment_parent>'+str(parent)+'</wp:comment_parent>\n\t</wp:comment>')
+        #if parent:
+        #    text.append('\n\t\t<wp:comment_parent>'+parent+'</wp:comment_parent>')
+        #text.append('\n\t</wp:comment>')
+
+    return ''.join(text), comment_id
+
+def postAnalyze(url, wordpress_admin):
+    """Analyze the post page to get almost all information needed
+    except for comments which will be done in another function"""
+
+    request = urllib2.Request(url, None, headers)
+    page = urllib2.urlopen(request).read()
+    #headers['Referer'] = url
+    print 'Fuck1'
+
+    post_title = post_title_pattern.search(page).group(1)
+    post_time = post_time_pattern.search(page).group(1)
+    post_content = post_content_pattern.search(page).group(1)
+    post_content = content_replace_pattern.sub('', post_content)
+    post_content = content_space_replace_pattern.sub(' ', post_content)
+    post_content = post_content.lstrip().rstrip()
+    print 'Fuck2'
+    if post_content[-6:] == '</div>': 
+        post_content = post_content[:-6].rstrip()
+
+    result = post_category_pattern.search(page)
+    if result:
+        post_category = result.group(1)
+    else: 
+        post_category='Uncategorized'
+
+    if r'<div class="allComm">' in page:
+        comment = 'open'
+    else:
+        comment = 'closed'
+    print 'Fuck3'
+
+    text = ['\n<item>\n\t<title>'+post_title+'</title>\n\t<dc:creator>' + str(wordpress_admin) + '</dc:creator>\n\t<content:encoded><![CDATA['+post_content+']]></content:encoded>\n\t<wp:post_date>'+post_time+'</wp:post_date>\n\t<wp:comment_status>'+comment+'</wp:comment_status>\n\t<wp:status>publish</wp:status>\n\t<wp:post_type>post</wp:post_type>\n\t<wp:is_sticky>0</wp:is_sticky>\n\t<category domain="category" nicename="'+urllib.quote(post_category)+'"><![CDATA['+post_category+']]></category>']
+
+    result = post_tags_pattern.search(page)
+    if result: 
+        post_tags = result.group(1)
+        for tag in post_tags.split(','):
+            text.append('\n\t<category domain="post_tag" nicename="'+urllib.quote(tag)+'"><![CDATA['+tag+']]></category>')
+    print 'Fuck4'
+    
+    return ''.join(text), comment
+
+def contentAnalyze(url):
+    """Analyze the contents to collect the urls of all posts"""
+    request = urllib2.Request(url, None, headers)
+    page=urllib2.urlopen(request).read()
+    posts_urls = post_pattern.findall(page)
+    nextpage_result=nextpage_pattern.search(page)
+    if nextpage_result:
+        #headers['Referer'] = url
+        nextpage_url = nextpage_result.group(1)
+    else:
+        nextpage_url = None
+    return posts_urls, nextpage_url 
+
+def indexAnalyze(url):
+    """Analyze the index to find out the url of the contents"""
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+    urllib2.install_opener(opener)
+
+    #headers['Referer'] = url
+    request = urllib2.Request(url, None, headers)
+    page=urllib2.urlopen(request).read()
+
+    sina_admin = sina_admin_pattern.search(page).group(1).decode('utf8')
+    content_url = contents_pattern.search(page).group(1)
+
+    return content_url, sina_admin
